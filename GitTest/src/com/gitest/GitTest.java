e94e5e2eeb5799c704df1e9fa04818fcc93dde4c
M.ELBEKKOUCH <M.ELBEKKOUCH@DESKTOP-DO8OHUG>
2020-01-06 22:13:33 +0100

    mon premier commit

---
+++ GitTest/src/com/gitest/GitTest.java
@@ -0,0 +1,10 @@
+package com.gitest;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("helo world !!!");
+	}
+
+}
---
+++ GitTest/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author M.ELBEKKOUCH
+ *
+ */
+module GitTest {
+}
